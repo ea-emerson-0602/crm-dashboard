e1237b72d33d09697ea48ab9f5f551cf0cbe1538
Aghogho Emerson Emokiniovo <aghoghoemerson7@gmail.com>
2024-12-20 23:55:20 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  // ... other config
+  env: {
+    browser: true, // This tells ESLint that the code will run in a browser environment
+    node: true, // This tells ESLint that the code will also run in Node.js (for SSR, etc.)
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   rules: {
-    'no-undef': 'off', // Turns off the no-undef rule entirely
-    //or for only specific files:
-    overrides: [
-      {
-        files: ["someFile.js"],
-        rules: {
-          "no-undef": "off"
-        }
-      }
-    ]
+    'no-undef': 'off', // Disable the "no-undef" rule to allow window usage
+    'react/react-in-jsx-scope': 'off', // Optional: if you use React 17+ and JSX without importing React
+    // Add other custom rules here if needed
   },
 };
